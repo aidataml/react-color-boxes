4228552d642101d1a0617a1618ce1817c88d0c5e
aidataml <52681210+aidataml@users.noreply.github.com>
2024-03-08 12:04:40 -0500

    Initial upload of src files

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <main>
+    <BoxContainer/>
+  </main>
+</template>
+
+<script>
+import BoxContainer from './components/BoxContainer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    BoxContainer,
+  },
+};
+</script>
+
+<style>
+  body {
+    margin: 0;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+  }
+
+  main {
+    display: flex;
+    align-items: center;
+    min-height: 100vh;
+    justify-content: center;
+  }
+</style>
